3323b45b3a907959b148faf566739c4005ad8c0f
FricoSimon <simonfriko@gmail.com>
2023-05-11 23:11:53 +0700

    first commit

---
+++ model/Post/post.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Title is required!'],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!'],
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: [true, 'Category is required!']
+    },
+    views: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    dislikes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User is required!']
+    },
+    photo: {
+        type: String,
+        required: [true, 'Photo is required!']
+    },
+},
+    {
+        timestamps: true
+    })
+const post = mongoose.model('Post', postSchema)
+module.exports = post
---
+++ model/User/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'Firstname is required!']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Lastname is required!']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required!'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required!'],
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    role: {
+        type: String,
+        enum: ['User', 'Admin', 'Editor'],
+    },
+    viewedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    followers: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    following: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    },
+}, {
+    timestamps: true
+})
+
+const user = mongoose.model('User', userSchema)
+module.exports = user
