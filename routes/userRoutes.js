3ba0b612435bf7a3ff45f1a59e48fc3f01326d4a
FricoSimon <simonfriko@gmail.com>
2023-07-05 22:32:54 +0700

    fix route error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,16 @@
+const express = require('express');
 const userRouter = express.Router();
 const { userRegister, userLogin, userGetById, userDelete, userUpdate } = require('../controllers/userControls');
 
 // user routes
-userRouter.post('register', userRegister);
+userRouter.post('/register', userRegister);
 
-userRouter.post('login', userLogin);
+userRouter.post('/login', userLogin);
 
-userRouter.get('profile/:id', userGetById);
+userRouter.get('/profile/:id', userGetById);
 
-userRouter.delete('profile/:id', userDelete);
+userRouter.delete('/profile/:id', userDelete);
 
-userRouter.put('profile/:id', userUpdate);
+userRouter.put('/profile/:id', userUpdate);
 
 module.exports = userRouter;
